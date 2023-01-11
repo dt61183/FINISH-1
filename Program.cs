3c0957daae0183b786da43a82fc1617a6feb994f
dt61183 <dt61183@gmail.com>
2023-01-11 17:19:46 +0700

    Решение

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿string[] array = new string[] { "tuk-tuk", "9", "Novosib", ";)", "$ %" };
+
+void PrintArray(string[] arr)
+{
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void Diffarray(string[] arr)
+{
+       for (int i = 0; i < arr.Length; i++)
+    {
+        string element = arr[i];
+        if (element.Length <= 3)
+        {
+            arr[i] = element;
+        }
+        else arr[i] =  string.Empty;
+
+    }
+}
+
+PrintArray(array);
+
+Diffarray(array);
+
+Console.Write("-> ");
+
+PrintArray(array);
